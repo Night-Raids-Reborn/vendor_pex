bd68e0105bf94ecc19e0a30a7d4c56b92361b4d0
darknius09 <darkniuslightness@gmail.com>
2023-03-23 06:38:28 +0000

    vendor: add adblock

--- config/common.mk
+++ config/common.mk
@@ -208,6 +208,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# AICP Ad-block
+PRODUCT_PACKAGES += \
+    hosts.aicp_adblock
+
 # Audio
 $(call inherit-product, vendor/aosp/config/audio.mk)
 
